82fe123e54079642b68a152bb0dfb2e07544812f
ilmi-veliu <iveliu011@gmail.com>
2025-04-08 09:33:04 +0200

    ajout de l ex 3 ,5

--- binary_trees.h
+++ binary_trees.h
@@ -1,20 +1,53 @@
-#ifndef BINARY_TREES_H
-#define BINARY_TREES_H
-
-#include <stdlib.h>
+#ifndef MAIN_H
+#define MAIN_H
+ 
 #include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
 
-/* structure de l’arbre */
-typedef struct binary_tree_s
+/**
+ * struct binary_tree_s - Binary tree node
+ *
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ */
+struct binary_tree_s
 {
-	int n;
-	struct binary_tree_s *parent;
-	struct binary_tree_s *left;
-	struct binary_tree_s *right;
-} binary_tree_t;
+    int n;
+    struct binary_tree_s *parent;
+    struct binary_tree_s *left;
+    struct binary_tree_s *right;
+};
 
-/* Prototypes */
+/* Basic Binary Tree */
+typedef struct binary_tree_s binary_tree_t;
+/* Binary Search Tree */
+typedef struct binary_tree_s bst_t;
+/* AVL Tree */
+typedef struct binary_tree_s avl_t;
+/* Max Binary Heap */
+typedef struct binary_tree_s heap_t;
+/* For BT visualisation */
+void binary_tree_print(const binary_tree_t *);
+
+/* Julia's tasks*/
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
+int binary_tree_is_leaf(const binary_tree_t *node);
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
+size_t binary_tree_depth(const binary_tree_t *tree);
+size_t binary_tree_leaves(const binary_tree_t *tree);
+int binary_tree_balance(const binary_tree_t *tree);
+/* needed for task 14 but to suppr later */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
+size_t binary_tree_height(const binary_tree_t *tree);
+void binary_tree_delete(binary_tree_t *tree);
+int binary_tree_is_root(const binary_tree_t *node);
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
 
+
+/* */
 #endif
