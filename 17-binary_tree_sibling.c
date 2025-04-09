b2404db3e5007fe7e586798005180740b3302274
JuliaCostaDeSousa <costadesousa.julia@gmail.com>
2025-04-09 10:00:53 +0200

    fix betty

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -4,7 +4,8 @@
  * binary_tree_sibling - Retourne le frère ou la sœur d'un nœud
  * @node: pointeur vers le nœud dont on veut le frère/la sœur
  *
- * Return: pointeur vers le frère ou la sœur, ou NULL si pas de frère ou de sœur
+ * Return: pointeur vers le frère ou la sœur
+ * ou NULL si pas de frère ou de sœur
  */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
